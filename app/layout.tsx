247cd2df4a116f3993c85b6b27124f6098a66cad
Egor B. <egor.bezriadin.01@gmail.com>
2026-02-16 18:10:38 -0800

    chore: speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Toaster } from 'sonner';
 import { ThemeProvider } from '@/components/theme-provider';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -28,6 +29,7 @@ export default async function RootLayout({
         >
           <Toaster position="top-center" />
           {children}
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
